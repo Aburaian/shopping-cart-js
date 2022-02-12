e2cc295b430dc2f11760476694d294691342308f
raiansakil <aburaian182@gmail.com>
2022-02-13 03:00:33 +0600

    added

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
-function numberValue(added) {
-    let numberAdded = document.getElementById("number-value");
+function numberValue(products, price, isAdded) {
+    let numberAdded = document.getElementById(products + "-number");
     let number = numberAdded.value;
-    if (added == true) {
+    if (isAdded == true) {
         numberAdded.value = Number(number) + 1;
 
     } else if (number > 0) {
         numberAdded.value = Number(number) - 1;
     }
+    document.getElementById(products + "-added").innerText = numberAdded.value * price;
+    calculateTotal();
 
 }
 
+// value added
+
 function added() {
-    numberValue(true)
+    numberValue("value", 59, true)
 
 
 }
 
 function minus() {
-    numberValue(false)
+    numberValue("value", 59, false)
 
+}
+
+// phone added
+
+function phoneAdded() {
+    numberValue("phone", 1219, true)
+
+
+}
+
+function phoneMinus() {
+    numberValue("phone", 1219, false)
+
+
+
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal() {
+    const phoneTotal = getInputValue("phone") * 1219;
+    const valueTotal = getInputValue("value") * 59;
+    const subTotal = phoneTotal + valueTotal;
+    const tax = subTotal / 10;
+    const totalAmount = subTotal + tax;
+    document.getElementById("sub-total").innerText = subTotal;
+    document.getElementById("tax").innerText = tax;
+    document.getElementById("total-amount").innerText = totalAmount;
 }
